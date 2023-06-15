ed25fcd9daa986a676f30eb8f430092e700fed62
Kish <67649347+Kishor-Patil97@users.noreply.github.com>
2023-06-15 22:05:30 +0200

    Data from MongoDB is now shown on the Box Office Page

--- frontend/src/pages/box-office/BoxOffice.jsx
+++ frontend/src/pages/box-office/BoxOffice.jsx
@@ -22,14 +22,17 @@ const BoxOfficePage = () => {
   return (
     <div className="boxOffice">
       <ContentWrapper>
-        <span className="bigText">Box Office</span>
+        <span className="bigText">
+          <span role="img" aria-label="money">💰</span> Box Office <span role="img" aria-label="money">💰</span>
+        </span>
         <span className="smallText">
           {boxOfficeData.map((movie) => (
             <div key={movie._id}>
-              <h2>Production: {movie.Production}</h2>
+              <br />
+              <h1>Production: {movie.Production}</h1>
+              <h2>Movie: {movie.Movie}</h2>
               <p>Total Revenue: {movie["Total Revenue"]}</p>
               <p>Number of Releases: {movie["Number of Releases"]}</p>
-              <p>Movie: {movie.Movie}</p>
               <p>Lifetime Gross: {movie["Lifetime Gross"]}</p>
             </div>
           ))}
