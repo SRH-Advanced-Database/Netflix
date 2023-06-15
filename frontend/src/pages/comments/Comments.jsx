477508879d209de609d54f7584ba21d4865a7f48
Kish <67649347+Kishor-Patil97@users.noreply.github.com>
2023-06-16 01:29:52 +0200

    data is shown from mongo db in the comments section

--- frontend/src/pages/comments/Comments.jsx
+++ frontend/src/pages/comments/Comments.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./style.scss";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
-const CommentsPage = () => {
+const Comments = () => {
   const [commentsData, setCommentsData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchComments = async () => {
       try {
         const response = await fetch("http://localhost:3000/get_reddit_comments");
         const data = await response.json();
@@ -16,24 +16,23 @@ const CommentsPage = () => {
       }
     };
 
-    fetchData();
+    fetchComments();
   }, []);
 
   return (
-    <div className="commentsData">
+    <div className="comments">
       <ContentWrapper>
         <span className="bigText">Comments</span>
-        <span className="smallText">
-          {commentsData.map((comments) => (
-            <div key={comments._id}>
-            <h2>Title: {comments.title}</h2>
-            <p>Score: {comments.score}</p>
-            <p>ID: {comments.id}</p>
-            <p>URL: {comments.url}</p>
-            <p>Created: {comments.created}</p>
-            <p>Body: {comments.body}</p>
-            <p>Timestamp: {comments.timestamp}</p>
-          </div>
+        <span className="commentText">
+          {commentsData.map((comment) => (
+            <div key={comment.id}>
+              <h2>{comment.title}</h2>
+              <p>Score: {comment.score}</p>
+              <p>URL: {comment.url}</p>
+              <p>Number of Comments: {comment.comms_num}</p>
+              <p>Created: {comment.created}</p>
+              <p>Timestamp: {comment.timestamp}</p>
+            </div>
           ))}
         </span>
       </ContentWrapper>
@@ -41,4 +40,4 @@ const CommentsPage = () => {
   );
 };
 
-export default CommentsPage;
+export default Comments;
